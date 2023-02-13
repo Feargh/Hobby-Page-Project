f8b0d808497dbc5b10df9fe543b543b882a81c06
Fearghal Kavanagh <121862261+Feargh@users.noreply.github.com>
2023-02-13 09:12:18 +0000

    Adding javascript code to call the date

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-let d = new Date();
-document.body.innerHTML =  d;
+n =  new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("date").innerHTML = d + "/" + m + "/" + y;
+
+//let d = new Date();
+//document.body.innerHTML =  d;
+//document.getElementById(date).innerHTML = d;
